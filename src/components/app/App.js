7ffe32bc10ec261366331d99e02630866524577a
d-e-k-k <davideberhart8@gmail.com>
2020-10-28 09:51:19 -0400

    Reset Components to generic template

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,18 +1,9 @@
-import React, {useState} from 'react';
-import Header from '../header/Header'
-import MadLibForm from '../mad-lib-form/MadLibForm';
+import React from 'react';
 
 const App = () => {
-    const [keyWords, setKeyWords] = useState([])
     return (
         <div>
-            <Header />
-            <main>
-                <MadLibForm 
-                    keyWords={keyWords}
-                    setKeyWords={setKeyWords}
-                />
-            </main>
+            
         </div>
     );
 };
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,15 +2,9 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <header>
-            <h1>Rap-Libs</h1>
-            <nav>
-                <ul>
-                    <li>New Rap</li>
-                    <li>About</li>
-                </ul>
-            </nav>
-        </header>
+        <div>
+            
+        </div>
     );
 };
 

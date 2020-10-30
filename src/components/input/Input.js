ade58cf8e72e00eb31628328fc62a70e812ef579
d-e-k-k <davideberhart8@gmail.com>
2020-10-30 14:28:34 -0400

    Removed key variable from Input.js that contained enviornmental variable. Current api does not require a key but the previous did

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Input = ({ setValues }) => {
 	function handleBlur(event) {
-		const key = process.env.REACT_APP_RHYME_API_KEY;
 			let inputValueLowerCase = event.target.value.toLowerCase();
 			const url = `https://rhymebrain.com/talk?function=getRhymes&word=${inputValueLowerCase}`;
 			if(!event.target.value){

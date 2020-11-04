ad6f7efb0d1128e182b8b367702e56d9bfe0fe3c
d-e-k-k <davideberhart8@gmail.com>
2020-11-04 08:42:34 -0500

    Added bolding for userInput words and key words when the rap is displayed

--- src/components/MadLibResults.js
+++ src/components/MadLibResults.js
@@ -10,24 +10,24 @@ const MadLibResults = ({ parent, InputListData, rapDisplayed }) => {
 						<source src={StillB} type='audio/mpeg' />
 					</audio>
 					<p>React is a pain, </p>
-					<p>In the {parent[0].userInput}</p>
+					<p>In the <span style={{fontWeight: "bold"}} >{parent[0].userInput}</span></p>
 					<p>It made my week long,</p>
-					<p>And my brain {parent[0].rhymingWord[Math.floor(Math.random()*5)]}</p>
+					<p>And my brain <span style={{fontWeight: "bold"}} >{parent[0].rhymingWord[Math.floor(Math.random()*5)]}</span></p>
 					<br />
 					<p>Change your state,</p>
-					<p>Like I change my {parent[1].userInput}.</p>
+					<p>Like I change my <span style={{fontWeight: "bold"}} >{parent[1].userInput}</span>.</p>
 					<p>Change my name,</p>
-					<p>Call me "{parent[1].rhymingWord[Math.floor(Math.random()*5)]}"</p>
+					<p>Call me "<span style={{fontWeight: "bold"}} >{parent[1].rhymingWord[Math.floor(Math.random()*5)]}</span>"</p>
 					<br />
 					<p>Full of hooks,</p>
 					<p>Like a takle box.</p>
 					<p>Full of lingo,</p>
 					<p>Where-ya docs?</p>
 					<br />
-					<p>Rap game {parent[2].userInput}</p>
-					<p>Code game {parent[2].rhymingWord[Math.floor(Math.random()*5)]}</p>
-					<p>useState {parent[3].userInput}</p>
-					<p>useEffect {parent[3].rhymingWord[Math.floor(Math.random()*5)]}</p>
+					<p>Rap game <span style={{fontWeight: "bold"}} >{parent[2].userInput}</span></p>
+					<p>Code game <span style={{fontWeight: "bold"}} >{parent[2].rhymingWord[Math.floor(Math.random()*5)]}</span></p>
+					<p>useState <span style={{fontWeight: "bold"}} >{parent[3].userInput}</span></p>
+					<p>useEffect <span style={{fontWeight: "bold"}} >{parent[3].rhymingWord[Math.floor(Math.random()*5)]}</span></p>
 					<br />
 					<p>No new props,</p>
 					<p>Just hand-me-downs</p>
